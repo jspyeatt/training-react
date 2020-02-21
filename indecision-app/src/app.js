50dc1dbf9d6e513dca3e1859e36c2e6b46b2126c
John Pyeatt <john.pyeatt@singlewire.com>
2020-02-21 09:21:16 -0600

    started adding styling with BEM and normalize.css

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import validator from 'validator';  // note we don't have ./ in front from 'validator'
                                     // this flags the system to find the library from node_modules
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 // sass-lang.com for scss.

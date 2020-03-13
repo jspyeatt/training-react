c53bd59679979082afb6e1f8133360bd63ad8fbb
John Pyeatt <john.pyeatt@singlewire.com>
2020-03-13 08:08:37 -0500

    early commit for expensify

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import validator from 'validator';  // note we don't have ./ in front from 'validator'
-                                    // this flags the system to find the library from node_modules
-import IndecisionApp from './components/IndecisionApp';
+// this flags the system to find the library from node_modules
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 // sass-lang.com for scss.
-
-ReactDOM.render(<p>This is my boiler plate.</p>, document.getElementById('appHere'));
+const ExpenseDashboardPath = () => (
+    <div>
+        some text
+    </div>
+);
+const routes = (
+    <BrowserRouter>
+        <Route path="/" component={ExpenseDashboardPath} />
+    </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById("appHere"));
